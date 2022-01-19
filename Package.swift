47f1cdadf870d1e7453164b77cdb1570b7cc6f58
ahead <info@ahead-itec.com>
2022-01-19 18:02:10 +0100

    MEPiSDK-Package v15.1.0

--- Package.swift
+++ Package.swift
@@ -4,39 +4,41 @@
 import PackageDescription
 
 private class Versions {
-    static let CMiTP = "15.0.0"
+    static let CMiTP = "15.1.0"
     static let ClientCertificates = "4.0.5"
     static let CommunicatorFramework = "6.0.0"
     static let CoreUtilities = "3.2.3"
-    static let FSi = "15.0.0"
+    static let FSi = "15.1.0"
     static let JOSESwift = "2.0.5"
-    static let MEPi = "15.0.0"
-    static let MEPiCommons = "15.0.0"
+    static let MEPi = "15.1.0"
+    static let MEPiCommons = "15.1.0"
     static let SecureStorage = "5.0.1"
     static let TalsecCertificatesFramework = "9.1.0"
     static let OpenSSL = "1.1.1200"
-    static let CMiWebView = "15.0.0"
+    static let CMiWebView = "15.1.0"
+    static let TalsecAttestation = "1.1.0"
 }
 
 private class Checksums {
-    static let CMiTP = "9c8c0abfde6b5be26c5d7016e6e11143cb01e846c4b8dce09faa85220a1e2eba"
+    static let CMiTP = "112bc4ed78698a75659c5584b7ae90f7bda93b5af8ba0f4d43b466b92c9d79db"
     static let ClientCertificates = "b7bc4ce959be1b496c99daf0c0238b0f224550c9245a04a062dde287343788bc"
     static let CommunicatorFramework = "c2a6d0ea1ef39531d244e8c41b95b4baa24179c4b283d986a577e9b18f6a1aba"
     static let CoreUtilities = "bf41861bb6b26c825e8066dc514a4ff4883c18a2ae7daf8429da4d9c80909ca3"
-    static let FSi = "185bb8d09f256ea3dd851d002b9a6a275753cc961833b472eb836a5a5912e844"
+    static let FSi = "850d91970d4d8ec79810e28fe47ae7c2c647e735426d279b13c774eb39251498"
     static let JOSESwift = "84d97de8fabc4038c1f2055c07a59da54e7de420a00aba519df08a3f93cd7e58"
-    static let MEPi = "ab0a65794bd34df9ce0393812c7031924538390b544b6a2db7ce49bdc983c124"
-    static let MEPiCommons = "563b2817a2f5b3eb24dacb12fdbabdae4f8743abbd62f6467431b611a0588e25"
+    static let MEPi = "c37973a415001642c1ff4bca6e0d957b9a21a16ae4d3b8cb3c4f488e083366e7"
+    static let MEPiCommons = "eb23fc5a196f70f7673bd15a2760bf11533b88e5d06287dcf0ae69d262e6e285"
     static let SecureStorage = "2f192c5d85a33b2914e9e40909ef89d74328d504e9d82c505e0965fa5f424dbf"
     static let TalsecCertificatesFramework = "d4fb69ae1a64ef44cc34bc4149f746ba9db154cd18a76fce3dc3b60e58af28ba"
     static let OpenSSL = "c253bcaf1ff5b22f544a5d7642dce422e84258a4f11833293a1d9b0334800108"
-    static let CMiWebView = "f378f612e9b08d6315cb6a7fb0661eee2a74ce0f11c30f4383afc53bebdfa64e"
+    static let CMiWebView = "90bf933e69afa147158f2f6f7594ece33f9686fb21e0fa89d37a8839a9b49089"
+    static let TalsecAttestation = "568cc12114a90bf7694fbb14f0381c3dacc030fe59c6fa3277064f22dc7a106c"
 }
 
 let package = Package(
     name: "MEPiSDK",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v14)
     ],
     products: [
         .library(
@@ -52,7 +54,8 @@ let package = Package(
                 "MEPiCommons",
                 "SecureStorage",
                 "TalsecCertificatesFramework",
-                "OpenSSL"
+                "OpenSSL",
+                "TalsecAttestation"
             ]
         ),
         .library(
@@ -82,6 +85,7 @@ let package = Package(
         .binaryTarget(name: "JOSESwift", url: assembleUrl("utils", "JOSESwift", Versions.JOSESwift), checksum: Checksums.JOSESwift),
         .binaryTarget(name: "SecureStorage", url: assembleUrl("talsec", "SecureStorage", Versions.SecureStorage), checksum: Checksums.SecureStorage),
         .binaryTarget(name: "TalsecCertificatesFramework", url: assembleUrl("talsec", "TalsecCertificatesFramework", Versions.TalsecCertificatesFramework), checksum: Checksums.TalsecCertificatesFramework),
+        .binaryTarget(name: "TalsecAttestation", url: assembleUrl("talsec", "TalsecAttestation", Versions.TalsecAttestation), checksum: Checksums.TalsecAttestation),
         .binaryTarget(name: "OpenSSL", url: assembleUrl("utils", "OpenSSL", Versions.OpenSSL), checksum: Checksums.OpenSSL),
         .binaryTarget(name: "Talsec",
                       url: "https://nexus3-public.monetplus.cz/repository/ahead-ios-release/com/aheaditec/talsec/Talsec/0.2.0/Talsec.xcframework.zip",

0e7452d31bc20342b1f3bf8b7fffb1770fd36ab1
Info Ahead <info@ahead-itec.com>
2022-10-31 13:37:21 +0100

    MEPiSDK-Package v18.2.2

--- Package.swift
+++ Package.swift
@@ -4,34 +4,34 @@
 import PackageDescription
 
 private class Versions {
-    static let CMiTP = "18.2.1"
+    static let CMiTP = "18.2.2"
     static let ClientCertificates = "6.0.0"
-    static let CommunicatorFramework = "7.1.0"
+    static let CommunicatorFramework = "7.1.1"
     static let CoreUtilities = "4.1.0"
-    static let FSi = "18.2.1"
+    static let FSi = "18.2.2"
     static let JOSESwift = "2.0.6"
-    static let MEPi = "18.2.1"
-    static let MEPiCommons = "18.2.1"
-    static let SecureStorage = "7.1.0"
+    static let MEPi = "18.2.2"
+    static let MEPiCommons = "18.2.2"
+    static let SecureStorage = "7.1.1"
     static let TalsecCertificatesFramework = "11.0.0"
     static let OpenSSL = "1.1.1200"
-    static let CMiWebView = "18.2.1"
+    static let CMiWebView = "18.2.2"
     static let TalsecAttestation = "3.0.1"
 }
 
 private class Checksums {
-    static let CMiTP = "689fd2dfb60af5fd859627271b4c100bfe6d93cf3248e7ee4ff1f6d6eb38602e"
+    static let CMiTP = "3401069ba2a4bed8d6634b99aea0e9d6ba54dfcef59653313bcf2a5e8d08c5cb"
     static let ClientCertificates = "f87f57891c83c9a92150f1a66c637a67261d09dbe3cf77590c3909edff5250fb"
-    static let CommunicatorFramework = "926d1725100ac401a03f6d28eaa6bdc7bef82524a5e74da55d3d5f5bc8064b66"
+    static let CommunicatorFramework = "aefd32f0e5f7068d77d49a8f03ce3b5fb860ce5408fed0f51bbe4fc9778a1aac"
     static let CoreUtilities = "7ca72ff7251255bd613b12207529a024b525a79a728d354e2d70b5ec37c232ba"
-    static let FSi = "0343a7cfaa4a5341a25ac8031445d6ea464425a0d5ff9383376842dde4d7a895"
+    static let FSi = "e23a1a60d60df5f27da45adb70211f7aa829084c9a356c92270bdfede1728e72"
     static let JOSESwift = "9a8f1d1b3d63d1c6ad3d67cc9ae34a1bdbadb9004ed244ea0e5eeef9b8e2dcd8"
-    static let MEPi = "4347499112e17efe888b754492669961383bc6d6dfc45717b22b64db0ac96a3c"
-    static let MEPiCommons = "9cb19fc66f34a306771bfeb18a232b553711f5162f3ab946916e7ec53894c0e1"
-    static let SecureStorage = "158c888ea49dbe06222299d6b5d6102601e03262adbaa9d0bfad8ef3ba78d404"
+    static let MEPi = "7dfeac2f66aea267d494e0752ba669f4c404e09c2b76a1882fbf632d60dcb671"
+    static let MEPiCommons = "afce7e729d204e4e2838fea21a7c5720e00e0b27c580379d968d74a28f3e6073"
+    static let SecureStorage = "ab32cbed9a07c6c4ac938f6737ef5a0b414366927f84f2958024d397809f983f"
     static let TalsecCertificatesFramework = "98d1b1def31a036e47793a7297f6217d066313047c9449e2cbef369ad5ba76e6"
     static let OpenSSL = "c253bcaf1ff5b22f544a5d7642dce422e84258a4f11833293a1d9b0334800108"
-    static let CMiWebView = "31de0d9b45dfd0b10a47f382a8bcf4e6d8a9fc5a7f7f4955a641b3358143ffb7"
+    static let CMiWebView = "db2ab5929d87e229de7b3314c213f6ec86db2db700b50fb33a9881965f3fe624"
     static let TalsecAttestation = "35f2c1615122ee60a7649c8a6f8f22838ad58c83a1677ab1d66337e8d6f8fdd7"
 }
 
